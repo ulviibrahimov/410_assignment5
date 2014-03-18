4e74bb9c370431e1b04904c2f59d75e95407c214
ulviibrahimov <ulvi@ualberta.ca>
2014-03-17 20:22:49 -0600

    done. Please check readme file

---
+++ sockets.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Copyright (c) 2013-2014 Abram Hindle
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Mostly copied from Assignment 4 and Abram Hindle's Websocket Examples. 
+import flask
+from flask import jsonify 
+from flask import Flask, request
+from flask_sockets import Sockets
+import gevent
+from gevent import queue
+import time
+import json
+import os
+
+app = Flask(__name__)
+sockets = Sockets(app)
+app.debug = True
+clients = list()
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def put(self, v):
+        self.queue.put_nowait(v)
+
+    def get(self):
+        return self.queue.get()
+        
+        
+class World:
+    def __init__(self):
+        self.clear()
+        # we've got listeners now!
+        self.listeners = list()
+        
+    def add_set_listener(self, listener):
+        self.listeners.append( listener )
+
+    def update(self, entity, key, value):
+        entry = self.space.get(entity,dict())
+        entry[key] = value
+        self.space[entity] = entry
+        self.update_listeners( entity )
+
+    def set(self, entity, data):
+        self.space[entity] = data
+        self.update_listeners( entity )
+
+    def update_listeners(self, entity):
+        '''update the set listeners'''
+        for listener in self.listeners:
+            listener(entity, self.get(entity))
+
+    def clear(self):
+        self.space = dict()
+
+    def get(self, entity):
+        return self.space.get(entity,dict())
+    
+    def world(self):
+        return self.space
+
+myWorld = World()        
+
+def set_listener( entity, data ):
+    send_all(json.dumps({entity: data}))
+
+myWorld.add_set_listener( set_listener )
+
+def send_all_json(obj):
+    send_all( json.dumps(obj) )
+    
+def send_all(msg):
+    for client in clients:
+        client.put( msg )
+
+
+@app.route('/')
+def hello():
+    return flask.redirect("/static/index.html")
+
+def read_ws(ws,client):
+    try:
+        while True:
+            msg = ws.receive()
+            print "WS RECV: %s" % msg
+            if (msg is not None):
+                packet = json.loads(msg)
+                entity=packet.get('entity')
+                data=packet.get('data')
+                myWorld.set(entity,data)
+                send_all_json( packet )
+                
+            else:
+                break
+    except:
+        '''Done'''
+
+@sockets.route('/subscribe')
+def subscribe_socket(ws):
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn( read_ws, ws, client )  
+    try:
+        while True:
+            # block here
+            msg = client.get()
+            print (msg)
+            ws.send(msg)
+    except Exception as e:# WebSocketError as e:
+        print "WS Error %s" % e
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
+
+
+def flask_post_json():
+    '''Ah the joys of frameworks! They do so much work for you
+       that they get in the way of sane operation!'''
+    if (request.json != None):
+        return request.json
+    elif (request.data != None and request.data != ''):
+        return json.loads(request.data)
+    else:
+        return json.loads(request.form.keys()[0])
+
+@app.route("/entity/<entity>", methods=['POST','PUT'])
+def update(entity):
+    v = flask_post_json()
+    #print (v)
+    myWorld.set( entity, v )
+    e = myWorld.get(entity) 
+    return jsonify( e )
+
+@app.route("/world", methods=['POST','GET'])    
+def world():
+    return jsonify(myWorld.world())
+  
+@app.route("/entity/<entity>")    
+def get_entity(entity):
+    return jsonify(myWorld.get(entity))
+
+
+@app.route("/clear", methods=['POST','GET'])
+def clear():
+    myWorld.clear()
+    return jsonify(myWorld.world())
+
+
+
+if __name__ == "__main__":
+    ''' This doesn't work well anymore:
+        pip install gunicorn
+        and run
+        gunicorn -k flask_sockets.worker sockets:app
+    '''
+    app.run()
